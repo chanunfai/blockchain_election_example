a8b6020d6736f07146e0c3768c808c130e059139
chanunfai <chanunfai@gmail.com>
2018-07-26 09:27:56 -0700

    add class for candidates

--- contracts/election.sol
+++ contracts/election.sol
@@ -2,8 +2,21 @@ pragma solidity ^0.4.24;
 
 contract Election{
 	// Constructor
-	string public candidate;
+	struct Candidate{
+		uint id;
+		string name;
+		uint voteCount;
+	}
+
+	mapping (uint => Candidate) public candidates;
+
+	uint public candidatesCount;
 	function Election () public{
-		candidate = "candidate 1";
+		addCandidate("Felix");
+		addCandidate("Iris");
+	}
+	function addCandidate (string _name) private{
+		candidatesCount ++;
+		candidates[candidatesCount] = Candidate(candidatesCount,_name,0);
 	}
 }
